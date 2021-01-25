6129efc514ff0a27a42ff9a9108a1041bf0f73d1
yfktn <friskantoni@gmail.com>
2021-01-25 13:22:54 +0700

    Perbaharui lagi untuk penyimpanan tgl, hormati timezone!

--- components/FullCalendar.php
+++ components/FullCalendar.php
@@ -6,7 +6,19 @@ use LogicException;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use InvalidArgumentException;
 use Yfktn\EventGubernur\Models\EGItem as EventsModel;
-
+/**
+ * Hi ini catatan untuk component ini.
+ * - bahwa query dari fullcalendar untuk mendapatkan events pada rentang waktu yang ada
+ *   menggunakan format ISO8601, dengan variable start dan end
+ * - bahwa dari request tersebut memiliki format string ISO8601
+ * - bahwa untuk melakukan query ke database, maka lakukan terlebih dahulu perubahan
+ *   timezone dari TZ User Frontend -> TZ System
+ * - setelah didapatkan hasil dari database, lakukan kembali konversi ke TZ user frontend
+ *   yaitu TZ System -> TZ User Frontend
+ * - untuk mendapatkan hasil yang konsisten maka terkait penggunaan tanggal dan waktu 
+ *   pada inputan tanggal dan jam, tidak lagi ignoreTimeZone bernilai true
+ * @package Yfktn\EventGubernur\Components
+ */
 class FullCalendar extends ComponentBase
 {
 
@@ -29,37 +41,59 @@ class FullCalendar extends ComponentBase
     {
         $start = input('start');
         $end = input('end');
-        $frontEndTimeZone = config('yfktn.eventgubernur::defaultFrontEndTZ');
+        // dd($start, $end);
+        // trace_log($start, $end);
         $systemTZ = config("app.timezone");
-        $startTZ = Carbon::parse($start, $frontEndTimeZone)->copy()->timezone($systemTZ);
-        $endTZ = Carbon::parse($end, $frontEndTimeZone)->copy()->timezone($systemTZ);
-
+        // karena di tanggal yang diberikan waktu fullcalendar melakukan permintaan
+        // events, pada string yang diberikan sudah ada informasi timezone nya
+        // sehingga kita tidak perlu lagi melakukan proses setting manual timezone
+        $startTZ = Carbon::parse($start);
+        $endTZ = Carbon::parse($end);
+        $frontEndTimeZone = $startTZ->timezone;
+        // trace_log($startTZ, $endTZ);
+        // trace_sql();
         // dapatkan dari db
-        $events = EventsModel::whereBetween('tgl_mulai', [$startTZ, $endTZ])
+        $events = EventsModel::whereBetween('tgl_mulai', [
+                $startTZ->copy()->timezone($systemTZ), 
+                $endTZ->copy()->timezone($systemTZ)])
             ->get();
         // loop untuk melakukan render ke JSON nya
         $data = [];
+        $i = 0;
         foreach ($events as $e) {
-            $data['id'] = $e->id;
-            $data['title'] = $e->judul;
+            $satuHari = false;
+            $data[$i]['id'] = $e->id;
+            $data[$i]['title'] = $e->judul;
             $theStart = Carbon::parse("{$e->tgl_mulai} {$e->jam_mulai}", $systemTZ);
             if ($e->tgl_selesai == null) {
                 // satu hari
                 if ($e->jam_selesai == null) {
                     // satu hari?
-                    $theEnd = $theStart->copy()->setTime(23, 59);
+                    $theEnd = $theStart->copy();
+                    $satuHari = true;
                 } else {
                     $theEnd = $theStart->copy()->setTimeFromTimeString($e->jam_selesai);
                 }
             } else {
                 if ($e->jam_selesai == null) {
-                    $theEnd = Carbon::parse("{$e->tgl_mulai}", $systemTZ)->setTime(23, 59);
+                    $theEnd = Carbon::parse("{$e->tgl_selesai}", $systemTZ);
+                    $satuHari = true;
                 } else {
-                    $theEnd = Carbon::parse("{$e->tgl_mulai} {$e->jam_mulai}", $systemTZ);
+                    $theEnd = Carbon::parse("{$e->tgl_selesai} {$e->jam_selesai}", $systemTZ);
                 }
             }
-            $data['start'] = $theStart->timezone($frontEndTimeZone);
-            $data['end'] = $theEnd->timezone($frontEndTimeZone);
+            // trace_log($e->judul, $theStart, $theEnd);
+            // dapatkan, convert ke timezone si front end dan set formatnya
+            $data[$i]['start'] = $theStart->timezone($frontEndTimeZone)->toIso8601String();
+            $theEnd->timezone($frontEndTimeZone);
+            // kalau di set satu hari, maka set pada waktu time telah dirubah timezone nya!
+            if($satuHari) {
+                $theEnd->setTime(
+                    23, 59, 59
+                );
+            }
+            $data[$i]['end'] = $theEnd->toIso8601String();
+            $i = $i + 1;
         }
         return $data;
     }
