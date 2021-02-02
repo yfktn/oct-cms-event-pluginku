cfdb7ae9539291c017476864e698fb255f047e18
yfktn <friskantoni@gmail.com>
2021-02-02 20:55:49 +0700

    tambahkan catatan tentang agenda

---
+++ updates/builder_table_update_yfktn_eventgubernur_catatan_kegiatan.php
@@ -0,0 +1,27 @@
+<?php namespace Yfktn\EventGubernur\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+/**
+ * Rubah beberapa field menjadi nullable sebagaimana issu nomor 3
+ * @package Yfktn\EventGubernur\Updates
+ */
+class BuilderTableUpdateYfktnEventgubernurItemNullable extends Migration
+{
+    public function up()
+    {
+        Schema::table('yfktn_eventgubernur_item', function($table)
+        {
+            $table->text('agenda')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('yfktn_eventgubernur_item', function ($table) 
+        {
+            $table->dropColumn('agenda');
+        });
+    }
+}
